02de72509d77b22c1514d77fd8d326917d903fdf
Khanbaba112200 <qasd37352@gmail.com>
2025-08-06 03:00:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ARSLAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkVPYXAxUzFwZnd2TEhLK3g1TGpzKzdwcVVYV3lCYmJRL3NWNmNHbVgwYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ1dWTFZ2Ung0RXFmOGlhNVhqOStYdkR5NFpMS25sbWpOc3dZdTk2ZkVRYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjSGd5dGVJNkNWSmZHQ3NlZVJyWGM5TkIyWU9NazN3aDQ0dFQ2ODhSL0dJPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJac1lWcG1NejF0WExGN2dRQW5qeWRzemhab3RaMko3VnFYMmU4alpRK2ljPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlVFTEtJRisycmdMeGNTci9pU1MvVTVVaU1Td3FkNnJIWnNiK1VtVFdPM0E9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik4ybG9jNEIzbkVoVFFJdHVkYnVQdWhId3ZOSlgzY0t6L2lZaTd4OWJBbTQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK0hZUEVjRlZzR3JnczVrSVJkcnB4Umh6bU9mOTRicjFpc01MeGsrM1YyYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieXovRFpkUUxsdlJpekdtQjdHMDVxcGhXMEFQRnMrQzBrdGMxei8zSlFWTT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlpBMEFyQkRGZW4wcW5XcDZpcVZ5d1NCSWV2QjczREROazlBMCtVRzgzcC81bUt5SE1TTVJWeFNISmRaR3drdXAvczMwOU1odDlkS2I3bjhXaVFuOGpRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTAyLCJhZHZTZWNyZXRLZXkiOiJoYXJEM05iSlNuamJURG9YbUE5NnhYZjBTTGdVQ2Nxa1RNS05yZXduYnJjPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZXJlZCI6dHJ1ZSwicGFpcmluZ0NvZGUiOiJBREJQRzVWMSIsIm1lIjp7ImlkIjoiOTIzMTQ3NjgyODIyOjM0QHMud2hhdHNhcHAubmV0IiwibGlkIjoiMzUwOTkwMDk2NTkwNzk6MzRAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNNN3JyNjhGRUtEN3ljUUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJkTUcxT3poN2FpTmZGSy9LMFpyQVQ3VHFMbmpjTEtCL1g4TzNtSEpkcEZZPSIsImFjY291bnRTaWduYXR1cmUiOiJsSnViWTQxRTlZYjFDUWtNcHR1RXh5VG1Rc1NMZFpwaVlVeE9JdGF1VWE4Znc3VEQ2K3A5Q2ZmNkYwSG1rR0Nad2hRN1dPTlJubjYwb0NucFpXSXFDQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiNXBobVd4VFdBenpjeVJFV2E2eFhNclpFMVIzTW0vR1BwdS91WjArYmhES2wzeVBzTUVPQi83TWxLSjBqYng5WFA1RS9HVk1IcHhyK0toSHV2ZUMyaEE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMxNDc2ODI4MjI6MzRAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWFRCdFRzNGUyb2pYeFN2eXRHYXdFKzA2aTU0M0N5Z2YxL0R0NWh5WGFSVyJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0JJSUFnPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU0NDMwODgzLCJsYXN0UHJvcEhhc2giOiIyVjc3cVUifQ==",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -21,7 +21,7 @@ ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
 // change it to 'same' if you want to resend deleted message in same chat     
 WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "true",
 // make true to know who dismiss or promoted a member in group
 ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
@@ -37,11 +37,11 @@ AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // true to get auto status react
 STICKER_NAME: process.env.STICKER_NAME || "Arslan_MD",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045919",
 // add your bot owner number
@@ -55,13 +55,13 @@ LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*Arslan_MD*🇵🇰",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
+ANTI_BAD: process.env.ANTI_BAD || "true",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
 // make anti link true,false for groups 
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
